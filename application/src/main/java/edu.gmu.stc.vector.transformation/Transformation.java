9a94177e43ed30065446220a85eebd2ae49321c0
Fei Hu <hufei68@gmail.com>
2018-01-20 14:54:34 -0500

    Enable application module to support Java

---
+++ application/src/main/java/edu.gmu.stc.vector.transformation/Transformation.java
@@ -0,0 +1,8 @@
+package edu.gmu.stc.vector.transformation;
+
+/**
+ * Created by Fei Hu on 1/19/18.
+ */
+public class Transformation {
+
+}
---
+++ application/src/main/scala/edu/gmu/stc/vector/application/Overlap.scala
@@ -0,0 +1,8 @@
+package edu.gmu.stc.vector.application
+
+/**
+  * Created by Fei Hu on 1/18/18.
+  */
+object Overlap {
+
+}
