02e0ee91c170c355682dd461937599b2c768d380
Fei Hu <hufei68@gmail.com>
2018-01-24 11:20:31 -0500

    add an scala example

--- application/src/main/scala/edu/gmu/stc/vector/examples/OverlapExample.scala
+++ application/src/main/scala/edu/gmu/stc/vector/examples/OverlapExample.scala
@@ -1,8 +1,42 @@
 package edu.gmu.stc.vector.examples
 
-/**
-  * Created by Fei Hu on 1/23/18.
-  */
-object OverlapExample {
+import edu.gmu.stc.vector.application.Overlap.intersect
+import edu.gmu.stc.vector.application.TaskUtil.show_timing
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.datasyslab.geosparksql.utils.GeoSparkSQLRegistrator
 
+/**
+  * Created by Fei Hu.
+  */
+object OverlapExample extends App {
+  val LOG = Logger.getLogger("OverlapExample")
+
+  Logger.getLogger("org").setLevel(Level.WARN)
+  Logger.getLogger("akka").setLevel(Level.WARN)
+
+  LOG.info("Start GeoSpark Overlap Spatial Analysis Example")
+
+  val sparkSession: SparkSession = SparkSession.builder()
+    .master("local[6]")
+    .appName("GeoSpark-Analysis")
+    .getOrCreate()
+
+  //val sqlContext = new SQLContext(sc)
+  //val sparkSession: SparkSession = sqlContext.sparkSession
+  GeoSparkSQLRegistrator.registerAll(sparkSession.sqlContext)
+
+  val resourceFolder = "/Users/feihu/Documents/GitHub/GeoSpark/application/src/main/resources/data/Washington_DC/"
+  //val resourceFolder = "/user/root/data0119/"
+  val shpFile1 = resourceFolder + "Impervious_Surface_2015_DC"
+  val shpFile2 = resourceFolder + "Soil_Type_by_Slope_DC"
+  val outputFile = resourceFolder + "dc_overlayMap_intersect_" + System.currentTimeMillis() + ".geojson"
+
+  val numPartition = 12
+
+  val runtime = show_timing(intersect(sparkSession, shpFile1, shpFile2, numPartition, outputFile))
+  LOG.info(s"*** Runtime is : $runtime")
+  LOG.info("Finish GeoSpark Overlap Spatial Analysis Example")
+
+  sparkSession.stop()
 }
